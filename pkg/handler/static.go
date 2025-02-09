109b4f99647035bc1ff6287e3851687c187991be
MisaghMomeniB <jenabemisagh@gmail.com>
2025-02-09 12:20:52 +0330

    IndexHandler serves the index.html file

--- pkg/handler/static.go
+++ pkg/handler/static.go
@@ -5,3 +5,16 @@ import (
     "io/ioutil"
     "path/filepath"
 )
+
+// IndexHandler serves the index.html file
+func IndexHandler(w http.ResponseWriter, r *http.Request) {
+    // Serve the index.html file
+    filePath := filepath.Join("static", "index.html")
+    data, err := ioutil.ReadFile(filePath)
+    if err != nil {
+        http.Error(w, "Could not load HTML file", http.StatusInternalServerError)
+        return
+    }
+    w.Header().Set("Content-Type", "text/html")
+    w.Write(data)
+}
