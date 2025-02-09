6171734e119f01b1429dd18468d1a5b76aae6f2a
MisaghMomeniB <jenabemisagh@gmail.com>
2025-02-09 12:17:29 +0330

    In-memory store for posts

--- pkg/handler/posts.go
+++ pkg/handler/posts.go
@@ -7,3 +7,8 @@ import (
     "go-web-service/pkg/model"
     "time"
 )
+
+// In-memory store for posts (a simple example)
+var posts = []model.Post{
+    {ID: 1, Title: "First Post", Content: "This is the first blog post.", CreatedAt: time.Now()},
+}
