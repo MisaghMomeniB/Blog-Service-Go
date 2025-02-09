e26f339a66d15c293969fc8a5262e55514cdb03f
MisaghMomeniB <jenabemisagh@gmail.com>
2025-02-09 12:16:00 +0330

    Connect connects to the database and returns the connection

--- internal/databse/db.go
+++ internal/databse/db.go
@@ -6,3 +6,13 @@ import (
 
 	_ "github.com/lib/pq" // PostgreSQL driver
 )
+
+// Connect connects to the database and returns the connection
+func Connect() *sql.DB {
+	connStr := "user=username dbname=mydb sslmode=disable"
+	db, err := sql.Open("postgres", connStr)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return db
+}
