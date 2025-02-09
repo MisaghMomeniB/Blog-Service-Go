0b1d66763134c4c6f424786c3d1381e4528286ec
MisaghMomeniB <jenabemisagh@gmail.com>
2025-02-09 12:13:58 +0330

    Update

--- cmd/main.go
+++ cmd/main.go
@@ -8,15 +8,15 @@ import (
 )
 
 func main() {
-		// Serve static files like HTML, CSS, and JS
-		http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("./static"))))
+	// Serve static files like HTML, CSS, and JS
+	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("./static"))))
 
-		// Set up the /users route
-		http.HandleFunc("/users", handler.UsersHandler)
+	// Set up the /users route
+	http.HandleFunc("/users", handler.UsersHandler)
 
-		// Set up the root route for the HTML page
-		http.HandleFunc("/", handler.IndexHandler)
+	// Set up the root route for the HTML page
+	http.HandleFunc("/", handler.IndexHandler)
 
-		fmt.Println("Server is running on port http://localhost:8080/")
-		log.Fatal(http.ListenAndServe(":8080", nil)) // Start the server
+	fmt.Println("Server is running on port http://localhost:8080/")
+	log.Fatal(http.ListenAndServe(":8080", nil)) // Start the server
 }
