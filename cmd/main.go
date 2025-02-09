14d0871c3d085f12bc5192d2fd5845edd78a6591
MisaghMomeniB <jenabemisagh@gmail.com>
2025-02-09 12:13:41 +0330

    Serve static files like HTML, CSS, and JS,Set up the /users route,Set up the root route for the HTML page

--- cmd/main.go
+++ cmd/main.go
@@ -6,3 +6,17 @@ import (
 	"log"
 	"net/http"
 )
+
+func main() {
+		// Serve static files like HTML, CSS, and JS
+		http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("./static"))))
+
+		// Set up the /users route
+		http.HandleFunc("/users", handler.UsersHandler)
+
+		// Set up the root route for the HTML page
+		http.HandleFunc("/", handler.IndexHandler)
+
+		fmt.Println("Server is running on port http://localhost:8080/")
+		log.Fatal(http.ListenAndServe(":8080", nil)) // Start the server
+}
