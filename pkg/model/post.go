5ed074da71b1ec53c311a1a2e369b756fb7adec7
MisaghMomeniB <jenabemisagh@gmail.com>
2025-02-09 12:23:29 +0330

    Post struct represents a blog post with title, content, and creation date

--- pkg/model/post.go
+++ pkg/model/post.go
@@ -2,3 +2,10 @@ package model
 
 import "time"
 
+// Post struct represents a blog post with title, content, and creation date
+type Post struct {
+    ID        int       `json:"id"`
+    Title     string    `json:"title"`
+    Content   string    `json:"content"`
+    CreatedAt time.Time `json:"created_at"`
+}
